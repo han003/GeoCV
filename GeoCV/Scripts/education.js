3e583cb2f53acf95d1b8c2e3ef414f90070a594c
Joar Jacobsen <jagolegion@gmail.com>
2015-04-07 13:31:29 +0200

    Test

---
+++ GeoCV/Scripts/education.js
@@ -0,0 +1,23 @@
+﻿$('#education-add-btn').click(function () {
+
+    var school = $('#school-text').val();
+    var description = $('#education-text').val();
+    var from = $('#education-select-from').val();
+    var to = $('#education-select-to').val();
+
+    console.log('School: ' + school);
+    console.log('Description: ' + description);
+    console.log('Period: ' + from + ' - ' + to);
+
+    $.ajax({
+        url: '/Education/AddNewEducation',
+        data: { Skole: school, Beskrivelse: description, Fra: from, Til: to },
+        type: 'POST',
+        success: function (data) {
+            $('#school-text').val('');
+            $('#education-text').val('');
+            alert('temp alert box: lagt til!');
+        }
+    });
+
+});
